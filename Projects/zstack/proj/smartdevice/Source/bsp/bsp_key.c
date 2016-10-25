08aa619efec37f528dc8c111438b8d7eace6016c
yearnext <1668501573@qq.com>
2016-10-25 23:01:25 +0800

    修改按键检测处理程序

--- Projects/zstack/proj/smartdevice/Source/bsp/bsp_key.c
+++ Projects/zstack/proj/smartdevice/Source/bsp/bsp_key.c
@@ -26,6 +26,7 @@
 
 /* Includes ------------------------------------------------------------------*/
 #include "bsp_key.h"
+#include <string.h>
 
 /* Exported macro ------------------------------------------------------------*/
 /* Exported types ------------------------------------------------------------*/
@@ -41,20 +42,34 @@
 typedef enum
 {
 	KEY_DETECT,
-	KEY_PRESS_FILTER,
-	KEY_PRESS_EDGE,
+/*	KEY_FILTER,*/
 	KEY_PRESS,
-	KEY_RELEASE_FILTER,
-	KEY_RELEASE_EDGE,
 	KEY_RELEASE,
 }key_status_t;
 
 /**
  *******************************************************************************
- * @brief       按键检测功能函数指针
+ * @brief       按键消息包
+ * @note        功能未完善
  *******************************************************************************
  */
-typedef bool(*hal_key_func)(void);
+typedef struct
+{
+	key_value_t value;
+	key_message_t message;
+}key_t;
+
+/**
+ *******************************************************************************
+ * @brief       按键定时器
+ * @note        功能未完善
+ *******************************************************************************
+ */
+typedef struct
+{
+	uint16 status : 1;
+	uint16 count : 15;
+}key_timer_t;
 
 /* Private variables ---------------------------------------------------------*/
 /**
@@ -62,259 +77,258 @@ typedef bool(*hal_key_func)(void);
  * @brief       按键功能变量
  *******************************************************************************
  */
-static key_status_t KEY_STATUS[KEY_MAX];
-static key_message_t KEY_MESSAGE[KEY_MAX];
+static key_t key;
+/*static key_timer_t timer;*/
+
+/**
+ *******************************************************************************
+ * @brief       按键功能检测数据
+ * @note        功能未完善
+ *******************************************************************************
+ */
+static struct
+{
+	key_timer_t press_long;
+}press_detect;
 
 /* Private functions ---------------------------------------------------------*/
 /**
  *******************************************************************************
- * @brief       按键1初始化函数
- * @param       [in/out]  void
- * @return      [in/out]  true
+ * @brief       按键定时器处理函数
+ * @param       [in/out]  value   按键键值
+ * @return      [in/out]  bool    事件触发标志
  * @note        None
  *******************************************************************************
  */
-static bool KEY1_PORT_INIT( void )
+static bool key_press_timer_handler(key_value_t value)
 {
-    KEY1_PORT_SEL &= ~KEY1_PORT_BIT; 
-    KEY1_PORT_DIR &= ~KEY1_PORT_BIT;
-    
-    return true;
+	if (press_detect.press_long.status)
+	{
+		if (key_message_send(value, KEY_MESSAGE_LONG_PRESS) == true)
+		{
+			press_detect.press_long.status = 0;			
+		}
+		
+		return true;
+	} 
+	else
+	{
+		if (++press_detect.press_long.count >= KEY_LONG_RRESS_TIME)
+		{
+			press_detect.press_long.count = 0;
+			
+			if (key_message_send(value, KEY_MESSAGE_LONG_PRESS) == false)
+			{
+				press_detect.press_long.status = 1;	
+			}
+			
+			return true;
+		}
+	}
+
+	return false;
 }
 
 /**
  *******************************************************************************
- * @brief       按键1检测函数
+ * @brief       清除定时器数据
  * @param       [in/out]  void
- * @return      [in/out]  按键按下状态
+ * @return      [in/out]  void
  * @note        None
  *******************************************************************************
  */
-static bool KEY1_DETECT( void )
+static void clear_key_press_timer(void)
 {
-    return ((KEY1_READ())?(true):(false));
-}
-
-/**
- *******************************************************************************
- * @brief       按键2初始化函数
- * @param       [in/out]  void
- * @return      [in/out]  true
- * @note        None
- *******************************************************************************
- */
-static bool KEY2_PORT_INIT( void )
-{
-    KEY2_PORT_SEL &= ~KEY2_PORT_BIT; 
-    KEY2_PORT_DIR &= ~KEY2_PORT_BIT;
-    
-    return true;
-}
-
-/**
- *******************************************************************************
- * @brief       按键2检测函数
- * @param       [in/out]  void
- * @return      [in/out]  按键按下状态
- * @note        None
- *******************************************************************************
- */
-static bool KEY2_DETECT( void )
-{
-    return ((KEY2_READ())?(true):(false));
+	memset(&press_detect, 0, sizeof(press_detect));
 }
 
 /* Exported functions --------------------------------------------------------*/
 /**
  *******************************************************************************
- * @brief       读取按键消息
- * @param       [in/out]  type      按键功能
+ * @brief       发送按键消息
+ * @param       [in/out]  value     按键键值
  *              [in/out]  message   按键消息
- * @return      [in/out]  void
+ * @return      [in/out]  bool      发送结果
  * @note        None
  *******************************************************************************
  */
-void key_message_send(uint8 type, key_message_t message)
+bool key_message_send(key_value_t value, key_message_t message)
 {
-	if( KEY_MESSAGE[type] == KEY_MESSAGE_NONE )
-    {
-        KEY_MESSAGE[type] = message;
-    }
+	if ( key.value == KEY_VALUE_NOP && key.message == KEY_MESSAGE_NONE )
+	{
+		key.value = value;
+		key.message = message;
+
+		return true;
+	}
+
+	return false;
 }
 
 /**
  *******************************************************************************
  * @brief       读取按键消息
- * @param       [in/out]  按键类型
- * @return      [in/out]  按键消息
+ * @param       [in/out]  value     按键键值
+ *              [in/out]  message   按键消息
+ * @return      [in/out]  bool      读取结果
  * @note        None
  *******************************************************************************
  */
-key_message_t key_message_read(uint8 type)
+bool key_message_read(key_value_t *value, key_message_t *message)
 {
-	key_message_t message = KEY_MESSAGE[type];
-
-	KEY_MESSAGE[type] = KEY_MESSAGE_NONE;
-
-	return message;
-}
-
-/**
- *******************************************************************************
- * @brief       遍历按键函数
- * @param       [in/out]  按键功能
- * @return      [in/out]  void
- * @note        None
- *******************************************************************************
- */
-void key_foreach(bsp_key_func func)
-{
-	uint8 i;
-
-	for (i = 0; i < KEY_MAX; i++)
+	if (key.value == KEY_VALUE_NOP && key.message == KEY_MESSAGE_NONE)
 	{
-		func(i);
+		return false;
+	}
+	else
+	{
+		*value = key.value;
+		*message = key.message;
+
+		key.value = KEY_VALUE_NOP;
+		key.message = KEY_MESSAGE_NONE;
+	
+		return true;
 	}
 }
 
 /**
  *******************************************************************************
  * @brief       按键初始化函数
- * @param       [in/out]  按键类型
+ * @param       [in/out]  void
  * @return      [in/out]  void
  * @note        None
  *******************************************************************************
  */
-void hal_key_init(uint8 type)
+void hal_key_init(void)
 {
-    hal_key_func init[] = { KEY1_PORT_INIT, KEY2_PORT_INIT };
-    
-    init[type]();
-    
-	KEY_STATUS[type] = KEY_DETECT;
-	KEY_MESSAGE[type] = KEY_MESSAGE_NONE;
+	KEY1_PORT_INIT();
+	KEY2_PORT_INIT();
+
+	clear_key_press_timer();
+
+	key.value = KEY_VALUE_NOP;
+	key.message = KEY_MESSAGE_NONE;
 }
 
 /**
  *******************************************************************************
- * @brief       按键检测函数
- * @param       [in/out]  按键类型
+ * @brief       读取按键键值
+ * @param       [in/out]  void
+ * @return      [in/out]  读取到的键值
+ * @note        None
+ *******************************************************************************
+ */
+key_value_t get_key_value(void)
+{
+	if (KEY1_READ())
+	{
+		return KEY_VALUE_SWITCH;
+	}
+
+	if (KEY2_READ())
+	{
+		return KEY_VALUE_RESET;
+	}
+
+	return KEY_VALUE_NOP;
+}
+
+/**
+ *******************************************************************************
+ * @brief       按键检测程序
+ * @param       [in/out]  void
  * @return      [in/out]  void
  * @note        None
  *******************************************************************************
  */
-void hal_key_scan(uint8 type)
+void key_scan(void)
 {
-	hal_key_func detect[] = { KEY1_DETECT, KEY2_DETECT };
-	bool status = detect[type]();
+	static key_value_t key_filter_value = KEY_VALUE_NOP;
+	static key_status_t key_now_status = KEY_DETECT;
 
-	switch (KEY_STATUS[type])
+	key_value_t key_now_value = get_key_value();
+
+	switch (key_now_status)
 	{
-        case KEY_DETECT:
-            if (status == true)
-            {
-                KEY_STATUS[type] = KEY_PRESS_FILTER;
-            }
-            else
-            {
-                KEY_STATUS[type] = KEY_RELEASE_FILTER;
-            }
-            break;
-            /** 按键按下事件 */
-        case KEY_PRESS_FILTER:
-            if (status == true)
-            {
-                KEY_STATUS[type] = KEY_PRESS_EDGE;
-                key_message_send(type, KEY_MESSAGE_PRESS_EDGE);
-            }
-            else
-            {
-                KEY_STATUS[type] = KEY_DETECT;
-            }
-            break;
-        case KEY_PRESS_EDGE:
-            if (status == true)
-            {
-                KEY_STATUS[type] = KEY_PRESS;
-                key_message_send(type, KEY_MESSAGE_PRESS);
-            }
-            else
-            {
-                KEY_STATUS[type] = KEY_DETECT;
-            }
-            break;
-        case KEY_PRESS:
-            if (status == true)
-            {
-                KEY_STATUS[type] = KEY_PRESS;
-                key_message_send(type, KEY_MESSAGE_PRESS);
-            }
-            else
-            {
-                KEY_STATUS[type] = KEY_DETECT;
-            }
-            break;
-            /** 按键松开事件 */
-        case KEY_RELEASE_FILTER:
-            if (status == false)
-            {
-                KEY_STATUS[type] = KEY_RELEASE_EDGE;
-                key_message_send(type, KEY_MESSAGE_RELEASE_EDGE);
-            }
-            else
-            {
-                KEY_STATUS[type] = KEY_DETECT;
-            }
-            break;
-        case KEY_RELEASE_EDGE:
-            if (status == false)
-            {
-                KEY_STATUS[type] = KEY_RELEASE;
-                key_message_send(type, KEY_MESSAGE_RELEASE);
-            }
-            else
-            {
-                KEY_STATUS[type] = KEY_DETECT;
-            }
-            break;
-        case KEY_RELEASE:
-            if (status == false)
-            {
-                KEY_STATUS[type] = KEY_RELEASE;
-                key_message_send(type, KEY_MESSAGE_RELEASE);
-            }
-            else
-            {
-                KEY_STATUS[type] = KEY_DETECT;
-            }
-            break;
-        default:
-            break;
+		case KEY_DETECT:
+			if (key_now_value == key_filter_value)
+			{
+				if (key_now_value != KEY_VALUE_NOP)
+				{
+					clear_key_press_timer();
+					key_now_status = KEY_PRESS;
+					key_message_send(key_now_value, KEY_MESSAGE_PRESS_EDGE);
+				}
+				else
+				{
+					key_now_status = KEY_RELEASE;
+				}
+			}
+			else
+			{
+				key_filter_value = key_now_value;
+				key_now_status = KEY_DETECT;
+			}
+			break;
+		case KEY_PRESS:
+			if ( key_now_value == key_filter_value )
+			{
+				if ( key_press_timer_handler(key_now_value) == false )
+				{
+					key_message_send(key_now_value, KEY_MESSAGE_PRESS);
+				}
+			} 
+			else
+			{
+				clear_key_press_timer();
+				key_message_send(key_filter_value, KEY_MESSAGE_RELEASE_EDGE);
+				
+				key_filter_value = key_now_value;
+				key_now_status = KEY_DETECT;
+			}
+			break;
+		case KEY_RELEASE:
+			if (key_now_value == KEY_VALUE_NOP)
+			{
+
+			}
+			else
+			{
+				key_filter_value = key_now_value;
+				key_now_status = KEY_DETECT;
+			}
+			break;
+		default:
+			break;
 	}
 }
 
 /**
  *******************************************************************************
  * @brief       按键处理函数
- * @param       [in/out]  按键类型
+ * @param       [in/out]  void
  * @return      [in/out]  void
  * @note        None
  *******************************************************************************
  */
-void key_handler(uint8 type)
+void key_handler(void)
 {
-	key_message_t message = key_message_read(type);
-
-	switch (type)
+	key_t message;
+	
+	if (key_message_read(&message.value, &message.message) == true)
 	{
-		case KEY_1:
-            key1_message_handler(message);
-			break;
-		case KEY_2:
-            //key2_message_handler(message);
-			break;
-		default:
-			break;
+		switch (message.value)
+		{
+			case KEY_VALUE_SWITCH:
+				key_switch_handler(message.message);
+				break;
+			case KEY_VALUE_RESET:
+				//key_reset_handler(message.message);
+				break;
+			default:
+				break;
+		}
 	}
 }
 
--- Projects/zstack/proj/smartdevice/Source/bsp/bsp_key.h
+++ Projects/zstack/proj/smartdevice/Source/bsp/bsp_key.h
@@ -30,9 +30,15 @@
 /* Exported macro ------------------------------------------------------------*/
 /**
  *******************************************************************************
- * @brief       按键模块硬件抽象层
+ * @brief       按键模块配置层
  *******************************************************************************
  */
+// 按键扫描时间
+#define KEY_SCAN_TIME (10)
+#define KEY_TIME2COUNT(n) ((n)/KEY_SCAN_TIME)
+
+#define KEY_LONG_RRESS_TIME KEY_TIME2COUNT(300)
+
 // 按键动作
 #define KEY_ACTIVE_LOW  !
 #define KEY_ACTIVE_HIGH !!
@@ -43,29 +49,29 @@
 #define KEY1_PORT_SEL    P0SEL
 #define KEY1_PORT_DIR    P0DIR
 #define KEY1_PORT_BIT    BV(0)
-//#define KEY1_PORT_INIT() st(KEY1_PORT_SEL &= ~KEY1_PORT_BIT; KEY1_PORT_DIR &= ~KEY1_PORT_BIT;)
+#define KEY1_PORT_INIT() st(KEY1_PORT_SEL &= ~KEY1_PORT_BIT; KEY1_PORT_DIR &= ~KEY1_PORT_BIT;)
 #define KEY1_READ()      KEY_ACTIVE_LOW(KEY1_PORT)
 /** KEY2 P1_2 */
 #define KEY2_PORT        P1_2
 #define KEY2_PORT_SEL    P1SEL
 #define KEY2_PORT_DIR    P1DIR
 #define KEY2_PORT_BIT    BV(2)
-//#define KEY2_PORT_INIT() st(KEY2_PORT_SEL &= ~KEY2_PORT_BIT; KEY2_PORT_DIR &= ~KEY2_PORT_BIT;)
+#define KEY2_PORT_INIT() st(KEY2_PORT_SEL &= ~KEY2_PORT_BIT; KEY2_PORT_DIR &= ~KEY2_PORT_BIT;)
 #define KEY2_READ()      KEY_ACTIVE_LOW(KEY2_PORT)
 
 /* Exported types ------------------------------------------------------------*/
 /**
  *******************************************************************************
- * @brief       配置按键类型
+ * @brief       配置按键键值
  * @note        用户可编辑
  *******************************************************************************
  */
-enum KEY_TYPE_ENUM
+typedef enum
 {
-	KEY_1 = 0x00,
-	KEY_2,
-	KEY_MAX,
-};
+	KEY_VALUE_NOP = 0x00,
+	KEY_VALUE_SWITCH,
+	KEY_VALUE_RESET,
+}key_value_t;
 
 /**
  *******************************************************************************
@@ -78,35 +84,24 @@ typedef enum
 	KEY_MESSAGE_NONE,
 	KEY_MESSAGE_PRESS_EDGE,
 	KEY_MESSAGE_PRESS,
+	KEY_MESSAGE_LONG_PRESS,
 	KEY_MESSAGE_RELEASE_EDGE,
 	KEY_MESSAGE_RELEASE,
 }key_message_t;
-
-/**
- *******************************************************************************
- * @brief       按键功能函数指针
- *******************************************************************************
- */
-typedef void(*bsp_key_func)(uint8 type);
 
 /* Exported variables --------------------------------------------------------*/
 /* Private define ------------------------------------------------------------*/
 /* Private typedef -----------------------------------------------------------*/
 /* Private variables ---------------------------------------------------------*/
 /* Exported functions --------------------------------------------------------*/
-// 按键消息相关
-extern void key_message_send( uint8, key_message_t );
-extern key_message_t key_message_read( uint8 );
+extern bool key_message_send(key_value_t, key_message_t);
+extern bool key_message_read(key_value_t*, key_message_t*);
+extern void hal_key_init(void);
+extern key_value_t get_key_value(void);
+extern void key_scan(void);
+extern void key_handler(void);
 
-// 按键功能相关
-extern void key_foreach( bsp_key_func );
-extern void hal_key_init( uint8 );
-extern void hal_key_scan( uint8 );
-extern void key_handler( uint8 );
-
-// 按键处理相关
-extern void key1_message_handler( key_message_t );
-extern void key2_message_handler( key_message_t );
+extern void key_switch_handler(key_message_t);
 
 #endif      /* __BSP_KEY_H__ */
 
